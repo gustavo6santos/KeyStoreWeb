2c7f05b1383979bf89c35850c1bacfa04545a984
gustavogaps6.6 <gustavogaps6.6@gmail.com>
2024-06-16 23:41:43 +0100

    Update

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -8,57 +8,106 @@ import nav_dropdown from '../Assets/Web Icons/dropdown_icon.png'
 import user_icon from '../Assets/Web Icons/profile-user_64572.png'
 
 export const Navbar = () => {
+
     const [menu, setMenu] = useState("shop");
+
     const { getTotalCartItems } = useContext(ShopContext);
+
     const menuRef = useRef();
 
+    const authTokenExists = localStorage.getItem('auth-token');
+
+
     const dropdown_toggle = (e) => {
+
         menuRef.current.classList.toggle('nav-menu-visible');
+
         e.target.classList.toggle('open');
+
     }
 
+
     return (
+
         <div className='navbar'>
+
             <div className="nav-logo">
-               <Link to='/'><img src={logo} alt="" /></Link> 
-               
+
+                <Link to='/shop'><img src={logo} alt="" /></Link> 
+
             </div>
+
             <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+
             <ul ref={menuRef} className="nav-menu">
+
                 <li onClick={() => { setMenu("shop") }}>
-                    <Link style={{ textDecoration: 'none', color: menu === "shop" ? '#FF4141' : '#FFFFFF' }} to='/'>Shop</Link>
-                    {menu === "shop" ? <hr /> : <></>}
+
+                    <Link style={{ textDecoration: 'none', color: menu === "shop"? '#FF4141' : '#FFFFFF' }} to='/shop'>Shop</Link>
+
+                    {menu === "shop"? <hr /> : <></>}
+
                 </li>
+
                 <li onClick={() => { setMenu("pc") }}>
-                    <Link style={{ textDecoration: 'none', color: menu === "pc" ? '#FF4141' : '#FFFFFF' }} to='/pc'>Pc</Link>
-                    {menu === "pc" ? <hr /> : <></>}
+
+                    <Link style={{ textDecoration: 'none', color: menu === "pc"? '#FF4141' : '#FFFFFF' }} to='/pc'>Pc</Link>
+
+                    {menu === "pc"? <hr /> : <></>}
+
                 </li>
+
                 <li onClick={() => { setMenu("ps") }}>
-                    <Link style={{ textDecoration: 'none', color: menu === "ps" ? '#FF4141' : '#FFFFFF' }} to='/ps'>Ps</Link>
-                    {menu === "ps" ? <hr /> : <></>}
+
+                    <Link style={{ textDecoration: 'none', color: menu === "ps"? '#FF4141' : '#FFFFFF' }} to='/ps'>Ps</Link>
+
+                    {menu === "ps"? <hr /> : <></>}
+
                 </li>
+
                 <li onClick={() => { setMenu("xbox") }}>
-                    <Link style={{ textDecoration: 'none', color: menu === "xbox" ? '#FF4141' : '#FFFFFF' }} to='/xbox'>Xbox</Link>
-                    {menu === "xbox" ? <hr /> : <></>}
+
+                    <Link style={{ textDecoration: 'none', color: menu === "xbox"? '#FF4141' : '#FFFFFF' }} to='/xbox'>Xbox</Link>
+
+                    {menu === "xbox"? <hr /> : <></>}
+
                 </li>
+
                 <li onClick={() => { setMenu("nintendo") }}>
-                    <Link style={{ textDecoration: 'none', color: menu === "nintendo" ? '#FF4141' : '#FFFFFF' }} to='/nintendo'>Nintendo</Link>
-                    {menu === "nintendo" ? <hr /> : <></>}
+
+                    <Link style={{ textDecoration: 'none', color: menu === "nintendo"? '#FF4141' : '#FFFFFF' }} to='/nintendo'>Nintendo</Link>
+
+                    {menu === "nintendo"? <hr /> : <></>}
+
                 </li>
+
             </ul>
+
             <div className="nav-login-cart">
-                {localStorage.getItem('auth-token')
-                    ? <>
-                       <Link to='/profile'><img  src={user_icon} alt="" /></Link> 
+
+                {authTokenExists
+
+                   ? <>
+
+                        <Link to='/profile'><img  src={user_icon} alt="" /></Link> 
+
                         <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/') }}>Logout</button>
+
+                        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+
+                        <div className="nav-cart-count">{getTotalCartItems()}</div>
+
                     </>
+
                     : <Link to='/login'><button>Login</button></Link>}
 
-                <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-                <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
+
         </div>
+
     );
+
 };
+
 
 export default Navbar;
